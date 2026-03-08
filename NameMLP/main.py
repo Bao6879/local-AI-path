fb256e7c425f6bfc7a0b83ef8bc09139149b6ba3
Bao <20108791@mail.wit.ie>
2026-03-08 17:23:32 +0000

    Written data set up and settings.

--- NameMLP/main.py
+++ NameMLP/main.py
@@ -1,3 +1,43 @@
+import random
 import torch
 import torch.nn.functional as F
 
+names=open('names.txt', 'r').read().splitlines()
+
+iToC={0: '.'}
+idx=0
+for i in range(97, 123):
+    iToC[chr(i)]=idx
+    idx+=1
+cToI={s:i for i, s in iToC}
+
+#Settings:
+blockSize=3
+features=10
+random.seed(3108)
+trainSplitSize=0.8
+
+#Set up
+#Data
+random.shuffle(names)
+trX, trY=[], []
+tsX, tsY=[], []
+idx=0
+for w in names[:5]:
+
+    ctx=[0]*blockSize
+    for ch in w+'.':
+        if idx<=len(names)*trainSplitSize:
+            trX.append(ctx)
+            trY.append(cToI[ch])
+        else:
+            tsX.append(ctx)
+            tsY.append(cToI[ch])
+        ctx=ctx[1:]+ch
+    
+    idx+=1
+    
+trX=torch.tensor(trX)
+trY=torch.tensor(trY)
+tsX=torch.tensor(tsX)
+tsY=torch.tensor(tsY)
