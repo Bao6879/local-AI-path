e438b377790797b7c90c0fb21f9964a4e1a2e8fc
Bao <20108791@mail.wit.ie>
2026-03-17 22:01:19 +0000

    Changed MLP, fixing nan bug.

--- NameMLP/main.py
+++ NameMLP/main.py
@@ -18,7 +18,7 @@ seed=3108
 random.seed(seed)
 trainSplitSize=0.8
 hiddenLayerNeurons=200
-learningRates=[(0, 0.1), (25000, 0.05), (50000, 0.01), (100000, 0.001), (1e9, 0.001)] #Learning rate decay
+learningRates=[(0, 0.1), (75000, 0.05), (200000, 0.01), (500000, 0.005), (1000000, 0.001), (1e9, 0.0001)] #Learning rate decay
 batchSize=64
 
 #Set up
@@ -50,28 +50,76 @@ tsY=torch.tensor(tsY)
 #Neural net
 g=torch.Generator().manual_seed(seed)
 C=torch.randn((27, features), generator=g)
-w1=torch.randn((blockSize*features, hiddenLayerNeurons), generator=g)*0.1 #Hidden layer
-#b1=torch.randn((hiddenLayerNeurons), generator=g)*0.01
-w2=torch.randn((hiddenLayerNeurons, 27), generator=g)*0.01
-b2=torch.randn((27), generator=g)*0
 
-bngain=torch.ones((1, hiddenLayerNeurons))
-bnbias=torch.zeros((1, hiddenLayerNeurons))
+#Torchifying NN
+class Linear:
+    def __init__(self, nin, nout, bias=True):
+        self.weight=torch.randn((nin, nout), generator=g)
+        self.bias=torch.zeros(nout) if bias else None
+    def __call__(self, x):
+        self.out=x@self.weight
+        if self.bias is not None:
+            self.out+=self.bias
+        return self.out
+    def params(self):
+        return [self.weight]+([] if self.bias is None else [self.bias])
 
-parameters=[C, w1, w2, b2, bngain, bnbias]
+class BNorm:
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps=eps
+        self.momentum=momentum
+        self.training=True
+
+        self.gamma=torch.ones(dim)
+        self.beta=torch.zeros(dim)
+
+        self.runMean=torch.zeros(dim)
+        self.runVar=torch.ones(dim)
+    def __call__(self, x):
+        if self.training:
+            xmean=x.mean(0, keepdim=True)
+            xvar=x.var(0, keepdim=True)
+        else:
+            xmean=self.runMean
+            xvar=self.runVar
+        x=(x-xmean)/torch.sqrt(xvar+self.eps)
+        self.out=self.gamma*x+self.beta
+        if self.training:
+            with torch.no_grad():
+                self.runMean=(1-self.momentum)*self.runMean+self.momentum*xmean
+                self.runVar=(1-self.momentum)*self.runVar+self.momentum*xvar
+        return self.out
+    def params(self):
+        return [self.gamma, self.beta]
+
+class Tanh:
+    def __call__(self, x):
+        self.out=torch.tanh(x)
+        return self.out
+    def params(self):
+        return []
+
+#True MLP
+layers=[Linear(features*blockSize, hiddenLayerNeurons, bias=False), BNorm(hiddenLayerNeurons), Tanh(),
+        Linear(hiddenLayerNeurons, hiddenLayerNeurons, bias=False), BNorm(hiddenLayerNeurons), Tanh(),
+        Linear(hiddenLayerNeurons, hiddenLayerNeurons, bias=False), BNorm(hiddenLayerNeurons), Tanh(),
+        Linear(hiddenLayerNeurons, hiddenLayerNeurons, bias=False), BNorm(hiddenLayerNeurons), Tanh(),
+        Linear(hiddenLayerNeurons, 27, bias=False), BNorm(27)]
+
+parameters=[C]+[p for layer in layers for p in layer.params()]
 for p in parameters:
     p.requires_grad=True
 
-for i in range(200000):
+for i in range(2000000):
     #Batch
     ix=torch.randint(0, trX.shape[0], (batchSize, ))
 
     #Forward pass
     emb=C[trX[ix]]
-    h=emb.view(batchSize, blockSize*features)@w1 #linear layer
-    h=torch.tanh(bngain*(h-h.mean(0, keepdim=True))/(h.std(0, keepdim=True))+bnbias) #batch normalization + output of hidden layer
-    end=h@w2+b2 #End predictions
-    loss=F.cross_entropy(end, trY[ix]) 
+    x=emb.view(emb.shape[0], -1)
+    for layer in layers:
+        x=layer(x)
+    loss=F.cross_entropy(x, trY[ix]) 
 
     #Backward pass
     for p in parameters:
@@ -87,29 +135,22 @@ for i in range(200000):
         p.data+=-lr*p.grad
     if (i+1)%100000==0:
         print(i, loss.data.item())
-
-#Calibrate the batch norm at the end
-with torch.no_grad():
-    emb=C[trX]
-    h=emb.view(emb.shape[0], blockSize*features)@w1
-    bnmean=h.mean(0, keepdim=True)
-    bnstd=h.std(0, keepdim=True)
-
-
+    break
+    
 #Final training loss
 emb=C[trX]
-h=emb.view(emb.shape[0], blockSize*features)@w1
-h=torch.tanh(bngain*(h-bnmean)/bnstd+bnbias)
-end=h@w2+b2
-trLoss=F.cross_entropy(end, trY) 
+h=emb.view(emb.shape[0], -1)
+for layer in layers:
+    h=layer(h)
+trLoss=F.cross_entropy(h, trY) 
 print(f'Training loss: {trLoss.data}')
 
 #Test loss
 emb=C[tsX]
-h=emb.view(emb.shape[0], blockSize*features)@w1
-h=torch.tanh(bngain*(h-h.mean(0, keepdim=True))/bnstd+bnbias)
-end=h@w2+b2
-tsLoss=F.cross_entropy(end, tsY) 
+h=emb.view(emb.shape[0], -1)
+for layer in layers:
+    h=layer(h)
+tsLoss=F.cross_entropy(h, tsY) 
 print(f'Test loss: {tsLoss.data}')
 
 #Sampling
@@ -118,10 +159,12 @@ for _ in range(20):
     s=""
     ctx=[0]*blockSize
     while True:
-        emb=C[torch.tensor(ctx)]
-        h=torch.tanh(bngain*(emb.view(1, blockSize*features)@w1-bnmean)/bnstd+bnbias)
-        end=h@w2+b2
-        probs=F.softmax(end, dim=1)
+        emb=C[torch.tensor([ctx])]
+        print(emb.shape)
+        x=emb.view(emb.shape[0], -1)
+        for layer in layers:
+            x=layer(x)
+        probs=F.softmax(x, dim=1)
         ix=torch.multinomial(probs, num_samples=1, generator=g).item()
         ctx=ctx[1:]+[ix]
         s+=iToC[ix]
